ee44ec38ff436e482d6e1df2c562da2f99e6ca71
torinunna <torinunna9588@gmail.com>
2023-09-03 22:33:59 +0900

    Initial Commit

---
+++ TimerNotification/TimerNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerNotificationApp.swift
+//  TimerNotification
+//
+//  Created by YUJIN KWON on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct TimerNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
